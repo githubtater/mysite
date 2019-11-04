9b30fa48588575d6c6c2f1bde6da1828753d6f55
Roy Tate <roytate@hotmail.com>
2019-11-03 20:46:41 -0800

    updated code from exercise

--- polling/admin.py
+++ polling/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Poll
+
+admin.site.register(Poll)
+from django.contrib.auth.models import User
+
+users = User.objects.all()
+print(users)
