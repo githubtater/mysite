1bacae8a48fdfec8915cddfd68f372c2f681ca9f
Roy Tate <roytate@hotmail.com>
2019-12-12 19:59:10 -0800

    google login now working

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from blogging.views import stub_view, list_view, detail_view
 from blogging import views
@@ -16,5 +16,6 @@ urlpatterns = [
     path('', list_view, name="blog_index"),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
     path('', include(router.urls)),
+    re_path(r'^accounts/', include('allauth.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -36,10 +36,19 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'polling',
     'blogging',
     'rest_framework',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # ... include the providers you want to enable:
+    # 'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
+
 ]
 
 MIDDLEWARE = [
@@ -64,11 +73,21 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
                 'django.contrib.messages.context_processors.messages',
+                # Required by allauth template tags
+                # "django.core.context_processors.request",
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
@@ -126,3 +145,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
+
+SITE_ID = 1
